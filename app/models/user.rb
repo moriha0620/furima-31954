571d34cacdb2e7d4df1dd8d5aaa043ffa3c6854e
moriha0620 <skk0620367@gmail.com>
2021-02-13 20:06:12 +0900

    ユーザー管理機能の実装とテストコードの記述31954

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,10 +7,15 @@ class User < ApplicationRecord
   with_options presence: true do
     validates :nickname
     validates :password, format: {with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]/}
-    validates :last_name, format: {with: /\A[ぁ-んァ-ン一-龥々]/}
-    validates :first_name, format: {with: /\A[ぁ-んァ-ン一-龥々]/}
-    validates :last_name_kana, format: {with: /\A[ァ-ヶー－]+\z/}
-    validates :first_name_kana, format: {with: /\A[ァ-ヶー－]+\z/}
     validates :birthday
+    
+    with_options format: {with: /\A[ぁ-んァ-ン一-龥々]+\z/} do
+      validates :last_name
+      validates :first_name
+    end
+    with_options format: {with: /\A[ァ-ヶー－]+\z/} do
+      validates :last_name_kana 
+      validates :first_name_kana
+    end
   end
 end
