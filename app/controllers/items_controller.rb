d24e39ebfd624f61e323a404ba0d77f8081e32f6
moriha0620 <skk0620367@gmail.com>
2021-02-20 18:05:44 +0900

    商品詳細表示機能の実装１

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -17,6 +17,10 @@ class ItemsController < ApplicationController
     end
   end
 
+  def show
+    @item = Item.find(params[:id])
+  end
+
   private
   def item_params
     params.require(:item).permit(:product_name, :product_description, :category_id, :product_condition_id,
